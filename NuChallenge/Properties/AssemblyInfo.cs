0f9668bead6fd67d5a4ecc8a55ac0002b6ca57bb
MARKETTRACK/meet.soni <meetsoni01041996@gmail.com>
2019-07-08 15:19:44 +0530

    Added project files

--- NuChallenge/Properties/AssemblyInfo.cs
+++ NuChallenge/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("41a1cdb0-6f0d-4979-a66a-3b06db9297f4")]
+[assembly: Guid("cd14133f-f0e2-4624-9c12-bcd7b1cb791c")]
 
 // Version information for an assembly consists of the following four values:
 //
